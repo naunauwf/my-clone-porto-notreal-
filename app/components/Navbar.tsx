9b14371c487f7be9e8f367cc1b876587560bd4e3
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-06-22 02:08:45 +0700

    feat: adding icon logo and some styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import Logo from "@/public/icon-nav.webp";
+import Logo from "@/public/images/icon-nav.webp";
 
 export default function Navbar() {
   return (
-    <nav className="navbar flex items-center justify-between h-[7.3rem] mx-16 my-0 text-logo">
-      <div className="navbar-logo flex justify-center items-center gap-5 font-bold text-[2rem] tracking-[-0.15rem] text-logo">
+    <nav className="navbar flex items-center justify-between h-[7.3rem] mx-16 my-0 bg-main-white text-main-color">
+      <div className="navbar-logo flex justify-center items-center gap-5 font-bold text-[2rem] tracking-[-0.15rem] text-main-color">
         <Image src={Logo} alt="iconLogo" width={50} height={50} />
         <span>naufal</span>
       </div>
@@ -14,9 +14,9 @@ export default function Navbar() {
         <li>Google Ads Resources</li>
         <li>About Me</li>
         <li>Contact</li>
-        <li className="flex items-center gap-1">
+        <li className="flex items-center justify-center gap-1 leading-none">
           <svg
-            className="navbar-flag shadow-[0.5px_0.5px_1px_rgba(0,0,0,0.5)]"
+            className="navbar-flag shadow-[0.5px_0.5px_1px_rgba(0,0,0,0.5)] translate-y-[0.9px]"
             xmlns="https://www.w3.org/2000/svg"
             width={20}
             height={10}
